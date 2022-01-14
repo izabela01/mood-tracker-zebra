99a84bd6383988163f38103b85e7451c7bbdf860
izabela01 <izabeladrabek23@gmail.com>
2022-01-14 12:24:13 +0000

    Adding Model files for db construction

---
+++ MoodTracker/Models/MoodEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace MoodTracker.Models
+{
+    public class MoodEntry
+    {
+        public IdentityUser User { get; set; }
+        public string ID { get; set; }
+        public string UserID { get; set; }
+        public DateTime Date { get; set; }
+        public int MoodScore { get; set; }
+        public string Notes { get; set; }
+
+
+        public ICollection<MoodLookup> Moods { get; set; }
+
+    }
+
+
+}
