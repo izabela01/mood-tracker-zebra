d05799e3cb3686684f4ce9ee1940ae2218c98ed1
mini-ninja-64 <callum.hart@sky.uk>
2022-01-21 14:44:03 +0000

    Update index page to hide data

--- MoodTracker/Pages/MoodEntries/Index.cshtml.cs
+++ MoodTracker/Pages/MoodEntries/Index.cshtml.cs
@@ -2,29 +2,38 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using MoodTracker.Data;
 using MoodTracker.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
 
 namespace MoodTracker.Pages.MoodEntries
 {
+    [Authorize]
     public class IndexModel : PageModel
     {
-        private readonly MoodTracker.Data.ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public IndexModel(MoodTracker.Data.ApplicationDbContext context)
+        public IndexModel(ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public IList<MoodEntry> MoodEntry { get;set; }
 
         public async Task OnGetAsync()
         {
+            // No need for null checks, due to unreachable code path when user is not logged in which is forced by the Authorize attribute
+            string currentUserId = _userManager.GetUserId(User);
+
             MoodEntry = await _context.MoodEntries
-                .Include(m => m.User).ToListAsync();
+                .Include(m => m.User)
+                .Where(m => m.UserId == currentUserId)
+                .ToListAsync();
         }
     }
 }
