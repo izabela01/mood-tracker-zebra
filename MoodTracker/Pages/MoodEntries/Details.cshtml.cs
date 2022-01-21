b8b81b6282726cb51845196e4845032ae2229053
mini-ninja-64 <callum.hart@sky.uk>
2022-01-21 14:58:08 +0000

    Protect Details page

--- MoodTracker/Pages/MoodEntries/Details.cshtml.cs
+++ MoodTracker/Pages/MoodEntries/Details.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -10,13 +12,16 @@ using MoodTracker.Models;
 
 namespace MoodTracker.Pages.MoodEntries
 {
+    [Authorize]
     public class DetailsModel : PageModel
     {
-        private readonly MoodTracker.Data.ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public DetailsModel(MoodTracker.Data.ApplicationDbContext context)
+        public DetailsModel(ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public MoodEntry MoodEntry { get; set; }
@@ -28,8 +33,12 @@ namespace MoodTracker.Pages.MoodEntries
                 return NotFound();
             }
 
+            string currentUserId = _userManager.GetUserId(User);
+
             MoodEntry = await _context.MoodEntries
-                .Include(m => m.User).FirstOrDefaultAsync(m => m.Id == id);
+                .Include(m => m.User)
+                .Where(m => m.User.Id == currentUserId)
+                .FirstOrDefaultAsync(m => m.Id == id);
 
             if (MoodEntry == null)
             {
