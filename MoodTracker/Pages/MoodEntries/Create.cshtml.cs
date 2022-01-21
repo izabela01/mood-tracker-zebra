cd78582f56b3c7092dfc66a3cacd979720d36189
mini-ninja-64 <callum.hart@sky.uk>
2022-01-21 17:29:28 +0000

    Update create page to protect against bad actors

--- MoodTracker/Pages/MoodEntries/Create.cshtml.cs
+++ MoodTracker/Pages/MoodEntries/Create.cshtml.cs
@@ -2,14 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using MoodTracker.Data;
+using MoodTracker.Extensions;
 using MoodTracker.Models;
 
 namespace MoodTracker.Pages.MoodEntries
 {
+    [Authorize]
     public class CreateModel : PageModel
     {
         private readonly MoodTracker.Data.ApplicationDbContext _context;
@@ -28,19 +31,25 @@ namespace MoodTracker.Pages.MoodEntries
         [BindProperty]
         public MoodEntry MoodEntry { get; set; }
 
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://aka.ms/RazorPagesCRUD.
+
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid)
+            MoodEntry newMoodEntry = new MoodEntry();
+
+            if (await TryUpdateModelAsync<MoodEntry>(
+                newMoodEntry,
+                "MoodEntry",
+                m => m.Date, s => s.MoodScore, s => s.Notes))
             {
-                return Page();
+                newMoodEntry.UserId = User.GetId();
+
+                _context.MoodEntries.Add(newMoodEntry);
+
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
             }
 
-            _context.MoodEntries.Add(MoodEntry);
-            await _context.SaveChangesAsync();
-
-            return RedirectToPage("./Index");
+            return Page();
         }
     }
 }
