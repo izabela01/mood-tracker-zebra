f1c19b0362dd25a8391a8eb2b4cdffd8156677fc
mini-ninja-64 <callum.hart@sky.uk>
2022-01-21 18:15:38 +0000

    Update edit page for safety

--- MoodTracker/Pages/MoodEntries/Edit.cshtml.cs
+++ MoodTracker/Pages/MoodEntries/Edit.cshtml.cs
@@ -2,20 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MoodTracker.Data;
+using MoodTracker.Extensions;
 using MoodTracker.Models;
 
 namespace MoodTracker.Pages.MoodEntries
 {
+    [Authorize]
     public class EditModel : PageModel
     {
-        private readonly MoodTracker.Data.ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
 
-        public EditModel(MoodTracker.Data.ApplicationDbContext context)
+        public EditModel(ApplicationDbContext context)
         {
             _context = context;
         }
@@ -31,41 +34,50 @@ namespace MoodTracker.Pages.MoodEntries
             }
 
             MoodEntry = await _context.MoodEntries
-                .Include(m => m.User).FirstOrDefaultAsync(m => m.Id == id);
+                .Include(m => m.User)
+                .Where(m => m.UserId == User.GetId())
+                .FirstOrDefaultAsync(m => m.Id == id);
 
             if (MoodEntry == null)
             {
                 return NotFound();
             }
-           ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+
             return Page();
         }
 
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(int id)
         {
-            if (!ModelState.IsValid)
+            MoodEntry moodEntryToUpdate = await _context.MoodEntries.FindAsync(id);
+
+            if ((moodEntryToUpdate == null) &&
+                (moodEntryToUpdate.UserId == User.GetId()))
             {
-                return Page();
+                return NotFound();
             }
 
-            _context.Attach(MoodEntry).State = EntityState.Modified;
+            if (await TryUpdateModelAsync<MoodEntry>(
+                moodEntryToUpdate,
+                "MoodEntry",
+                m => m.Date, m => m.MoodScore, m => m.Notes))
+            {
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!MoodEntryExists(MoodEntry.Id))
+                try
                 {
-                    return NotFound();
+                    await _context.SaveChangesAsync();
                 }
-                else
+                catch (DbUpdateConcurrencyException)
                 {
-                    throw;
+                    if (!MoodEntryExists(MoodEntry.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
                 }
+
             }
 
             return RedirectToPage("./Index");
