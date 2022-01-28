44c9407e7de75ec6f3f5b34c6bc354fb1fde54b5
Callum Hart <mini-ninja-64@users.noreply.github.com>
2022-01-28 16:10:58 +0000

    Private data (#6)
    * Update index page to hide data
    
    * Protect Details page
    
    * Add extension for id
    
    * Protect details page
    
    * Update MoodEntries Index to use extension method
    
    * Update create page to protect against bad actors
    
    * Update edit page for safety
    
    * Remove commented out code blocks
    
    * Update Delete page to be check user id
    
    * Simplify expressions

---
+++ MoodTracker/Extensions/UserExtensions.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Security.Claims;
+
+namespace MoodTracker.Extensions
+{
+    public static class UserExtensions
+    {
+        public static string GetId(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
--- MoodTracker/Pages/MoodEntries/Index.cshtml.cs
+++ MoodTracker/Pages/MoodEntries/Index.cshtml.cs
@@ -2,29 +2,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using MoodTracker.Data;
 using MoodTracker.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using MoodTracker.Extensions;
 
 namespace MoodTracker.Pages.MoodEntries
 {
+    [Authorize]
     public class IndexModel : PageModel
     {
-        private readonly MoodTracker.Data.ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public IndexModel(MoodTracker.Data.ApplicationDbContext context)
+        public IndexModel(ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public IList<MoodEntry> MoodEntry { get;set; }
 
         public async Task OnGetAsync()
         {
-            MoodEntry = await _context.MoodEntries
-                .Include(m => m.User).ToListAsync();
+           MoodEntry = await _context.MoodEntries
+                .Include(m => m.User)
+                .Where(m => m.UserId == User.GetId())
+                .ToListAsync();
         }
     }
 }
